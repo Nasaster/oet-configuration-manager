934085c1f068535f931e064dc2677d4072d4bae7
thanasis tsokaktsidis <nas.tsokaktsidis@gmail.com>
2015-03-17 11:49:29 +0100

    everything in the main component for now

--- src/configuration-manager/configuration-manager.controller.js
+++ src/configuration-manager/configuration-manager.controller.js
@@ -1,26 +1,16 @@
 var pubsub = require('oet-lib/pubsub');
 var view = require('./configuration-manager.view');
-var configurationSelectors = require('./configuration-selectors/configuration-selectors.controller');
-var configurationAccordions = require('./configuration-accordions/configuration-accordions.controller');
 
 var ConfigurationManager = function(){};
 ConfigurationManager.prototype.constructor = ConfigurationManager;
 var configurationManager = module.exports = new ConfigurationManager();
 
-ConfigurationManager.prototype.init = function(config, wrapper){console.log(config);
+ConfigurationManager.prototype.init = function(config, wrapper){
 	this.$config = config || {};
     this.$wrapper = wrapper || document.body;
 
 
-    return view.init(this)
-    	.then( function(){
-    		// initialize selectors
-    		return configurationSelectors.init( { applications: Object.keys(this.$config) } , this.$DOM.selectorsSection );
-    	}.bind(this) )
-    	.then( function(){
-    		// initialize accordions
-    		return configurationAccordions.init( { channels: this.$config.channels }, this.$DOM.accordionsSection );
-    	}.bind( this ));
+    return view.init(this);
 };
 
 ConfigurationManager.prototype.broadcast = function(message, data){
--- src/configuration-manager/configuration-manager.view.js
+++ src/configuration-manager/configuration-manager.view.js
@@ -9,15 +9,25 @@ ConfigurationManagerView.prototype.init = function(controller){
     $scope = controller;
     //returns the promise created in templator.render
     return this.render($scope.$wrapper, $scope.$config)
-        .then(registerDOM)
-        // .then(registerEvents);
+        .then( registerDOM )
+        .then( function(){
+            return renderSelector($scope.$DOM.selectorsSection, { applications: Object.keys($scope.$config) } )
+        } )
+        .then(registerBehaviour);
 };
 
 //we expose the render method because there may come the need for the controller to render it again
-ConfigurationManagerView.prototype.render = function(wrapper, data){
+ConfigurationManagerView.prototype.render = function(wrapper, locals){
     //use the templator to render the html
-    return templator.render('views/configuration-manager/main.html', data, wrapper);
+    return templator.render('views/configuration-manager/main.html', locals, wrapper)
 };
+
+var renderSelector = function(wrapper, locals){
+    return templator.render('views/configuration-manager/application-selector.html', locals, wrapper)
+        .then(function(){
+            $scope.$DOM.applicationMenuItemsList = $scope.$wrapper.querySelector('.application-select ul.dropdown-menu');
+        })
+}
 
 //we cache all the DOM elements we'll use later
 var registerDOM = function(){
@@ -27,6 +37,19 @@ var registerDOM = function(){
 };
 
 //we attach the events needed
-var registerEvents = function(){
-    // $scope.$DOM.element.addEventListener('click', clickHandler);
+var registerBehaviour = function(){
+    $scope.$DOM.applicationMenuItemsList.addEventListener('click', function(ev){
+        var clickedElement = ev.target;
+        if( !clickedElement.classList.contains('menu-item') ){
+            return;
+        }
+
+        selectedApplicationLi && selectedApplicationLi.classList.remove('active');
+        selectedApplicationLi = clickedElement.parentNode;
+        
+        selectedApplicationLi.classList.add('active');
+        $scope.broadcast('selected application changed', {
+            selectedApplication: clickedElement.dataset.application
+        });  
+    });
 };
