28f5584e89cd035e1fb5bb1ba89e6a22099d3c75
André Alçada Padez <andre.padez@gmail.com>
2015-03-19 18:53:38 +0100

    form-groups are expanding and collapsing

--- src/configuration-manager/accordions/accordions.view.js
+++ src/configuration-manager/accordions/accordions.view.js
@@ -10,11 +10,12 @@ var accordionsView = module.exports = new AccordionsView();
 AccordionsView.prototype.init = function(controller){
     $scope = controller;
     //returns the promise created in templator.render
+    registerDOM();
+    registerBehaviour();
 };
 
 AccordionsView.prototype.changeSelectedApplication = function(){
     var config = $scope.$config.templates[ $scope.selectedApplication.toLowerCase() ].application;
-    config.selectedApplication = $scope.selectedApplication;
 
     templator.empty($scope.$wrapper)
         .then( formHelper.buildFormTree.bind(formHelper, config) )
@@ -24,11 +25,23 @@ AccordionsView.prototype.changeSelectedApplication = function(){
 var registerDOM = function(){
     this.$DOM = {};
     this.$DOM.fieldEditModal = $scope.$wrapper.querySelector('.field-edit-modal .modal-dialog');
-    this.$DOM.fieldGroupContainers = Array.prototype.slice.call( $scope.$wrapper.querySelectorAll('.group-form') );
+    
     console.log(this.$DOM);
 };
 
 var registerBehaviour = function(){
-
+    $scope.$wrapper.addEventListener('click', function(ev){
+        var clickedElement = ev.target;
+        switch(clickedElement.dataset.role){
+            case 'toggle-sub-group':
+                ev.preventDefault();
+                var ancestorLI = clickedElement.parentNode.parentNode;
+                var wasClosed = ancestorLI.classList.contains('closed');
+                ancestorLI.classList[ wasClosed? 'remove' : 'add' ]('closed');
+                ancestorLI.querySelector('i.action-open').classList[ wasClosed? 'add' : 'remove' ]('hidden');
+                ancestorLI.querySelector('i.action-close').classList[ wasClosed? 'remove' : 'add' ]('hidden');
+                break;
+        }
+    });
 };
 
