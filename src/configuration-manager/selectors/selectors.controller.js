d07467ba6fa2cf446b21b6e4a9e501859bb19d31
André Alçada Padez <andre.padez@gmail.com>
2015-03-27 10:44:54 +0100

    fixed bug

--- src/configuration-manager/selectors/selectors.controller.js
+++ src/configuration-manager/selectors/selectors.controller.js
@@ -10,7 +10,7 @@ Selectors.prototype.init = function(parent){
     this.$wrapper = this.$parent.$DOM.selectorsSection;
     return view.init(this)
         .then( this.selectApplication.bind(null, Object.keys(this.$parent.$config["application.json"])[0]) )
-        .then( this.selectConfigType.bind(null, this.$config.configTypes[0] ) );
+        .then( this.selectConfigType.bind(null, this.$parent.$config.configTypes[0] ) );
 };
 
 Selectors.prototype.selectApplication = function(selectedApplication){
