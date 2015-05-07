dceebf8a790b520f129afcbbd92534855145654a
thanasis tsokaktsidis <nas.tsokaktsidis@gmail.com>
2015-05-07 14:11:32 +0200

    Develop the configuration for locale.json

--- server/configuration-manager.js
+++ server/configuration-manager.js
@@ -12,14 +12,18 @@ module.exports = {
 	saveConfiguration: function(body){
 		switch( body.selectedConfigType ){
 			case 'application.json':
-				saveApplicationConfig( body.selectedApplication, body.configToBeSaved.channels);
+				saveApplicationConfig( body.selectedApplication, body.configToBeSaved.channels );
+				break;
+			case 'locale.json':
+				saveLocaleConfig( body.selectedLocale, body .configToBeSaved.channels );
 				break;
 		}
 	}, 
 
 	saveTemplate: function(body){
 		//[TODO] add logic to distinguish between templates
-		var path = configRoot + body.selectedApplication + '.' + body.selectedConfigType.replace('.json', '') +  '.template.json';
+		var path = configRoot + body.selectedApplication + '.' + body.selectedConfigType.replace( '.json', '' ) + '.template.json';
+		var path = configRoot + body.selectedLocale + '.' + body.selectedConfigType.replace( '.json', '' ) + '.template.json';
 		var jsonToWrite = JSON.stringify( body.templateToBeSaved, null, 4 );
 		fs.writeFileSync( path, jsonToWrite, {encoding: 'utf8'} );
 		setUpConfigurationObject();
@@ -33,6 +37,17 @@ var saveApplicationConfig = function(application, config){
 	Object.keys(config).forEach(function(channelName){
 		var filePath = baseDir + channelName + '/config/application.json';
 		var dataToBeSaved = JSON.stringify( config[channelName], null, 4 );
+		fs.writeFileSync( filePath, dataToBeSaved, {encoding: 'utf8'} );
+	});
+	setUpConfigurationObject();
+};
+
+var saveLocaleConfig = function(locale, locale){
+	var baseDir = configRoot + locale + '/channels/';
+
+	Object.keys(locale).forEach(function(channelName){
+		var filePath = baseDir + channelName + '/locale/locale.json';
+		var dataToBeSaved = JSON.stringify( locale[channelName], null, 4 );
 		fs.writeFileSync( filePath, dataToBeSaved, {encoding: 'utf8'} );
 	});
 	setUpConfigurationObject();
@@ -52,31 +67,39 @@ function setUpConfigurationObject(someParam)
 		});
 	};
 
+	var loadConfiguration = function(configType, appName) {
+		configObject.templates[appName] = ( configObject.templates[appName] ? configObject.templates[appName] : {} );
+		var json = fs.readFileSync(configRoot + appName + '.' + configType + '.template.json', {encoding: 'utf8'});
+		configObject.templates[appName][configType + '.json'] = JSON.parse(json);
+
+		configObject[configType + '.json'][appName] = { channels: {} };
+		configObject.channels = configObject.channels || filterFiles(configRoot + appName + '/channels/');
+		
+		configObject.channels.forEach(function(channelName){
+			var channelPath = configRoot + appName + '/channels/' + channelName;
+			if( fs.existsSync( channelPath + '/config/' + configType + '.json' ) ) {
+				var json = fs.readFileSync( channelPath + '/config/' + configType + '.json', {enconding: 'utf8'} );
+				configObject[configType + '.json'][appName].channels[channelName] = JSON.parse( json );
+			}
+		});
+	}
+
 	configObject = {
 		templates: {},
 		"application.json": {},
+		"locale.json": {},
 		channels: undefined,
 		configTypes: [
-			'application.json'
+			'application.json',
+			'locale.json'
 		]
 	};
 
 	var applications = filterFiles(configRoot);
 	
 	applications.forEach(function(appName){
-		configObject.templates[appName] = {};
-		var json = fs.readFileSync(configRoot + appName + '.application.template.json', {encoding: 'utf8'});
-		configObject.templates[appName]['application.json'] = JSON.parse(json);
-
-		configObject["application.json"][appName] = { channels: {} };
-		configObject.channels = configObject.channels || filterFiles(configRoot + appName + '/channels/');
-		
-		configObject.channels.forEach(function(channelName){
-			var channelPath = configRoot + appName + '/channels/' + channelName;
-			if( fs.existsSync(channelPath) ) {
-				var json = fs.readFileSync( channelPath + '/config/application.json', {enconding: 'utf8'} );
-				configObject["application.json"][appName].channels[channelName] = JSON.parse( json );
-			}
+		['application', 'locale'].forEach(function(configType) {
+			loadConfiguration(configType, appName);
 		});
 	});
 };
