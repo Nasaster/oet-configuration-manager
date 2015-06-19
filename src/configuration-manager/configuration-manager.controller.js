dce9df1efa2d368b7b1adcc13be10027f5bd1e5c
Péter Forgács <foripepe@yahoo.com>
2015-06-19 13:15:55 +0200

    XE-10550 Preparation for checking the configurations

--- src/configuration-manager/configuration-manager.controller.js
+++ src/configuration-manager/configuration-manager.controller.js
@@ -17,7 +17,22 @@ ConfigurationManager.prototype.init = function(config, wrapper){
     return view.init(this)
         .then( configurationProxy.init.bind(configurationProxy, this) )
         .then( accordions.init.bind(accordions, this) )
-        .then( selectors.init.bind(selectors, this) );
+        .then( selectors.init.bind(selectors, this) )
+        .then( this.checkConfigurations() );
+};
+
+ConfigurationManager.prototype.checkConfigurations = function(){
+    this.$issues = [];
+
+    this.checkConfigurationChannels();
+    this.checkConfigurationTemplates();
+};
+
+ConfigurationManager.prototype.checkConfigurationChannels = function(){
+    //console.warn(this.$config);
+};
+
+ConfigurationManager.prototype.checkConfigurationTemplates = function(){
 };
 
 ConfigurationManager.prototype.broadcast = function(message, data){
